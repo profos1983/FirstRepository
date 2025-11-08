81198ec7eba7426f315d63298bfb357ea697d046
profos1983 <jianko@hotmail.com>
2025-11-08 20:58:15 +0300

    Создание первого приложения 1

--- Task1-1/Program.cs
+++ Task1-1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string MyName = "Jane";
+Console.WriteLine(MyName);
+
+
+Console.ReadLine();
