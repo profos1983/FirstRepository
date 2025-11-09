3226b7421da748fd8e52c279ffd5cd62de1e06cc
profos1983 <jianko@hotmail.com>
2025-11-09 22:01:04 +0300

    Учеба 2025-11-09 часть 2

--- Таsk3-6/Program.cs
+++ Таsk3-6/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 
-double result = 5.0 / 2.0;
+double result = (double) 5 / (double) 2;
 Console.WriteLine("5 / 2 = {0}",
     arg0: result);
 
@@ -10,6 +10,26 @@ Console.WriteLine("Value: {0} Increment: {1}",
     arg1: counter++);
 
 double result2 = 10 % 3;
-Console.WriteLine("Результат вычисления остатка от деления 10 % 3 = {}",
+Console.WriteLine("Результат вычисления остатка от деления 10 % 3 = {0}",
     arg0: result2);
 
+Console.Write("Введите Ваше имя: ");
+string name = Console.ReadLine();
+Console.Write("Введите Ваш возраст: ");
+byte age = checked (byte.Parse(Console.ReadLine()));
+Console.Write("Какой ваш самый любимый день недели?");
+DayOfWeek favouritDay = (DayOfWeek) int.Parse(Console.ReadLine());
+Console.Write("Your favourite day is {0}", favouritDay);
+
+Console.WriteLine((int) favouritDay);
+
+enum DayOfWeek: int
+{
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7
+};
