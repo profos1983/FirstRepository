b7404cf04c1d2ab0828d33902cd92d7c21d52499
Алексей Миронов <Jianko@hotmail.com>
2025-11-11 21:34:05 +0300

    Домашняя работа 2025-11-11

---
+++ Task4-1/Program.cs
@@ -0,0 +1,22 @@
+﻿int A = 12;
+int B = 14;
+double X = 12.4;
+double Y = 45.4;
+bool C = false;
+
+C = (A < B) | (X > Y);
+
+Console.WriteLine(C);
+
+var a = 6;
+var b = 7;
+
+if (a != b)
+{
+    Console.WriteLine("Условие истинно");
+}
+else 
+{
+    Console.WriteLine("Условие ложно");
+}
+
