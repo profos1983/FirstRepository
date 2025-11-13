205ca431c5239ad17e9c68b543cfe00a62c66a90
profos1983 <jianko@hotmail.com>
2025-11-13 22:05:32 +0300

    2025-11-13 вторая часть

---
+++ Task4-4-1/Program.cs
@@ -0,0 +1,8 @@
+﻿(string name, int age) anketa;
+
+Console.Write("Напишите свое имя: ");
+anketa.name = Console.ReadLine();
+Console.Write("Напишите свой возраст: ");
+anketa.age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Выше имя: {0}, Ваш возраст: {1}. Приятно познакомиться!", anketa.name, anketa.age);
