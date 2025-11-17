6f93d97271210d41600e8e7790883a33827a5bbe
profos1983 <jianko@hotmail.com>
2025-11-17 18:48:00 +0300

    сделал на работе 2025-11-17

---
+++ Task 6-2-2/Program.Classes.cs
@@ -0,0 +1,82 @@
+﻿using System.Drawing;
+
+partial class Program
+{
+    public class Pen
+    {
+        #region Параметры
+        public string color;
+        public double cost;
+        #endregion
+
+        #region Конструкторы
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100.0;
+        }
+
+        public Pen (string penColor, double penCost)
+        {
+            color = penColor;
+            cost = penCost;
+
+        }
+        #endregion
+
+        #region Методы
+        public void ShowPenInfo()
+        {
+            Console.WriteLine("Цвет ручки: {0}, цена: {1}", color, cost);
+        }
+        #endregion
+    }
+
+    // Создайте класс Rectangle для представления прямоугольников удовлетворяющий следующим требованиям:
+    // Класс должен содержать целочисленные поля для сторон a и b.
+    // Класс должен содержать метод Square, возвращающий площадь прямоугольника (произведение сторон).
+    // Класс должен содержать 3 конструктора: с 2 параметрами, когда a != b, с 1 параметром, когда a == b, 
+    // и конструктор без параметров по умолчанию, в котором стороны будут заполняться как a = 6, b = 4.
+
+    public class Rectangle
+    {
+        #region Параметры класса
+        public int a;
+        public int b;
+        #endregion
+
+        #region Конструкторы
+        // Конструктор по умолчанию
+        public Rectangle() 
+        {
+            a = 6;
+            b = 4;
+        }
+
+        public Rectangle(int first, int second)
+        {
+            a = first;
+            b = second;
+        }
+
+        public Rectangle(int site)
+        {
+            a = site;
+            b = site;
+        }
+        #endregion
+
+        #region Методы
+        public int Square ()
+        {
+            return a * b;
+        }
+
+        public void ShowRectangleInfo()
+        {
+            Console.WriteLine("Длина: {0}, Ширина: {1}, Площадь: {2}", a, b, Square());
+        }
+        #endregion
+    }
+
+}
---
+++ Task 6-2-2/Program.cs
@@ -0,0 +1,16 @@
+﻿Pen pen1 = new();
+Pen pen2 = new("Синий", 56.0);
+Pen pen3 = new Pen {color = "Зеленый",cost = 43.0 }; // через инициализацию
+
+pen1.ShowPenInfo();
+pen2.ShowPenInfo();
+pen3.ShowPenInfo();
+
+Rectangle rec1 = new Rectangle();
+Rectangle rec2 = new Rectangle(6);
+Rectangle rec3 = new Rectangle(10,6);
+
+rec1.ShowRectangleInfo();
+rec2.ShowRectangleInfo();
+rec3.ShowRectangleInfo();
+
