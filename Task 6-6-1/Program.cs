3ac8922c4d427c9ed677a72b034cab9f8d06fee9
profos1983 <jianko@hotmail.com>
2025-11-18 16:54:18 +0300

    2025-11-18 часть 1

---
+++ Task 6-6-1/Program.cs
@@ -0,0 +1,55 @@
+﻿class TrafficLight
+{
+    private string color;
+
+    private void ChangeColor (string newcolor)
+    {
+        color = newcolor; 
+    }
+
+    public string GetColor()
+    {
+        return color;
+    }
+
+    class User
+    {
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Возраст должен быть не меньше 18");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                if (Email)
+            }
+
+        }
+    }
+}
+
