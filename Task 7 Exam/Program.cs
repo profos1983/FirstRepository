0c774c7f414cd4f0418388676c321bf36ec38a84
Алексей Миронов <Jianko@hotmail.com>
2025-11-22 23:07:37 +0300

    2025-11-22 часть 3

--- Task 7 Exam/Program.cs
+++ Task 7 Exam/Program.cs
@@ -35,3 +35,48 @@ class Order<TDelivery, TStruct> where TDelivery : Delivery
 
     // ... Другие поля
 }
+
+abstract class Person
+{
+    private string firstName;
+    public string FirstName
+    { 
+        get { return firstName; }
+        internal set { firstName = value; }
+    }
+
+    private string lastName;
+    public string LastName 
+    { 
+        get { return lastName; }
+        internal set { lastName = value; }
+    }
+
+    private string adress;
+    public string Adress
+    { 
+        get { return adress; }
+        internal set { adress = value; }
+    }
+
+    private string personId;
+    public string PersonId
+    {
+        get { return personId; }
+        protected set 
+        {
+            if (value.Length == 10 && value.Substring(0,2) == "ID")
+            {
+                personId = value;
+            }
+            else
+            {
+                Console.WriteLine("ID пользователя должно вводиться в виде: ID********, где * - любая цифра или буква");
+            }
+            
+        }
+    }
+
+
+
+}
