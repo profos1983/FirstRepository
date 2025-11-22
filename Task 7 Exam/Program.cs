0569dd1610ba6bf8cbe1afe786cff252a09fa1cf
Алексей Миронов <Jianko@hotmail.com>
2025-11-22 23:48:51 +0300

    2025-11-22 часть 3

--- Task 7 Exam/Program.cs
+++ Task 7 Exam/Program.cs
@@ -40,43 +40,45 @@ abstract class Person
 {
     private string firstName;
     public string FirstName
-    { 
+    {
         get { return firstName; }
         internal set { firstName = value; }
     }
 
     private string lastName;
-    public string LastName 
-    { 
+    public string LastName
+    {
         get { return lastName; }
         internal set { lastName = value; }
     }
 
     private string adress;
-    public string Adress
-    { 
+    public string PersonAdress
+    {
         get { return adress; }
         internal set { adress = value; }
     }
-
-    private string personId;
-    public string PersonId
-    {
-        get { return personId; }
-        protected set 
-        {
-            if (value.Length == 10 && value.Substring(0,2) == "ID")
-            {
-                personId = value;
-            }
-            else
-            {
-                Console.WriteLine("ID пользователя должно вводиться в виде: ID********, где * - любая цифра или буква");
-            }
-            
-        }
-    }
-
-
-
 }
+
+
+    class Client : Person
+    {
+        private string clientId;
+        public string ClientId
+    {
+            get { return clientId; }
+            protected set
+            {
+                if (value.Length == 10 && value.Substring(0, 2) == "ID")
+                {
+                    clientId = value;
+                }
+                else
+                {
+                    Console.WriteLine("ID пользователя должно вводиться в виде: ID********, где * - любая цифра или буква");
+                }
+
+            }
+        }
+
+    }
