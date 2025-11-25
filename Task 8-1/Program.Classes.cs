dabf4d1a4af20a29c26de693a0349a32751abbff
Алексей Миронов <Jianko@hotmail.com>
2025-11-25 22:14:55 +0300

    2025-11-25 часть 2

---
+++ Task 8-1/Program.Classes.cs
@@ -0,0 +1,49 @@
+﻿partial class Program
+{
+    Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+    public void CreateFolder(string name)
+    {
+        Folders.Add(name, new Folder());
+    }
+    public class DiskInfo
+    {
+        protected string diskName;
+        public string DiskName
+        {
+            get
+            {
+                return diskName;
+            }
+        }
+
+        protected long diskSize;
+        public long DiskSize
+        {
+            get
+            {
+                return diskSize;
+            }
+        }
+
+        protected long freeSize;
+        public long FreeSize
+        {
+            get
+            {
+                return freeSize;
+            }
+        }
+
+        public DiskInfo(string diskName, long diskSize, long freeSize)
+        {
+            this.diskName = diskName;
+            this.diskSize = diskSize;
+            this.freeSize = freeSize;
+        }
+    }
+
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+    }
+}
---
+++ Task 8-2/Program.cs
@@ -0,0 +1,14 @@
+﻿DriveInfo[] drives = DriveInfo.GetDrives();
+foreach (DriveInfo drive in drives)
+{
+    Console.WriteLine($"Название диска:    {drive.Name}.");
+    Console.WriteLine($"     Тип диска:    {drive.DriveType}.");
+    
+    if (drive.IsReady)
+    {
+        Console.WriteLine($"         Объем:    {drive.TotalSize}");
+        Console.WriteLine($"      Свободно:    {drive.TotalFreeSpace}");
+        Console.WriteLine($"         Метка:    {drive.VolumeLabel}");
+    }
+    Console.WriteLine("");
+}
