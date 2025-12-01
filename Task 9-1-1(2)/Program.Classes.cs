7343b3e0f5875f10f01430da11a53d070d3ba8b9
profos1983 <jianko@hotmail.com>
2025-12-01 16:16:50 +0300

    2025-12-01 часть 1

---
+++ Task 9-1-1(2)/Program.Classes.cs
@@ -0,0 +1,19 @@
+﻿partial class Program
+{
+    public static int? SimpleCalc(int x, int y)
+    {
+        try
+        {
+            return x / y;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return null ;
+        }
+        finally
+        {
+            Console.WriteLine("Блок finally сработал!");
+        }
+    }   
+}
---
+++ Task 9-1-1(2)/Program.cs
@@ -0,0 +1,34 @@
+﻿Exception myException = new("Мое собственное исключение");
+myException.Data.Add("Дата создания исключения: ", DateTime.Now);
+myException.HelpLink = "www.ya.ru";
+
+int x = 11;
+int y = 0;
+int? z = SimpleCalc(x, y);
+Console.WriteLine( "z равен: " + (z.HasValue ? z: "null"));
+
+try
+{
+    throw new ArgumentOutOfRangeException();
+}
+catch(ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Срабатывание блока finally.");
+}
+
+try
+{
+    throw new RankException();
+}
+catch (RankException ex)
+{
+    Console.WriteLine(ex.GetType());
+}
+finally
+{
+    Console.WriteLine("Срабатывание блока finally.");
+}
