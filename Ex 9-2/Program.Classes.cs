33ec748d131036815f6e39d68fee935523812087
profos1983 <jianko@hotmail.com>
2025-12-07 17:11:16 +0300

    2025-12-07 часть 1

--- Ex 9-2/Program.Classes.cs
+++ Ex 9-2/Program.Classes.cs
@@ -1,10 +1,47 @@
-﻿using static Program;
+﻿using System.Runtime.CompilerServices;
+using static Program;
 
 partial class Program
 {
+    /// <summary>
+    /// Делегат, который отслеживает ввод пользователем числа
+    /// </summary>
+    /// <param name="number">Число, введенное пользователем</param>
+    public delegate void UserChoiceNumber(int chosenNumber);
     public class ListOfPersons
     {
         private string[] listOfPersons;
+
+        // Медот, который будет реагировать на введенное пользователем число через делегат и событие.
+        public void OnUserChoiceNumber(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    this.CreateList();
+                    Console.WriteLine();
+                    Console.WriteLine("Введенный Вами список пользователей");
+                    this.PrintList();
+                    break;
+                case 1:
+                    this.SortList();
+                    Console.WriteLine();
+                    Console.WriteLine("Список пользователей после сортировки А-Я");
+                    this.PrintList();
+                    break;
+                case 2:
+                    this.ReverseSort();
+                    Console.WriteLine();
+                    Console.WriteLine("Список пользователей после сортировки Я-А");
+                    this.PrintList();
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    Console.WriteLine("Введенный Вами список пользователей");
+                    this.PrintList();
+                    break;
+            }
+        }
 
         public void CreateList()
         {
@@ -14,7 +51,7 @@ partial class Program
 
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("Введите имя " + num + " пользователя.");
+                Console.Write("Введите имя " + num + " пользователя: ");
                 listOfPersons[i] = Console.ReadLine();
                 num++;
             }
@@ -22,11 +59,10 @@ partial class Program
 
         public void PrintList()
         {
-            Console.WriteLine("Введеный список пользователей:");
-            Console.WriteLine("==============================");
+            Console.WriteLine("=================================");
             foreach (string s in listOfPersons)
             {
-                Console.WriteLine(s); 
+                Console.WriteLine(s);
             }
         }
 
@@ -50,9 +86,33 @@ partial class Program
         }
     }
 
+    /// <summary>
+    /// Класс, который генерирует событие ввода числа пользователем.
+    /// </summary>
+    public class UserChoice
+    {
+        // Объявляем событие выбора числа пользователем.
+        public event UserChoiceNumber ChoiceNumber;
+        private int number;
+        public int Number
+        {
+            get
+            {
+                return number; 
+            }
+            set
+            {
+                number = value;
+                //Публикация введенного пользователем числа.
+                if (value >= 0 && value <= 3)
+                    ChoiceNumber?.Invoke(number);
+            }
+        }    
+    }
+
     public static void Cleanconsole() 
     {
-        Console.WriteLine("Введете любой символ для возврата в меню");
+        Console.Write("Введете любой символ для возврата в меню");
         Console.ReadKey();
         Console.Clear();
     }
--- Ex 9-2/Program.cs
+++ Ex 9-2/Program.cs
@@ -1,81 +1,59 @@
-﻿ListOfPersons newList = new();
-
-while (true)
+﻿internal partial class Program
 {
-    Console.WriteLine("=================================");
-    Console.WriteLine("Введите следующие числа для:\n" +
-        "1 - сортировка введенного массива от А-Я\n" +
-        "2 - обратная сортировка введенного массива от Я-А\n" +
-        "3 - Формирование массива из пяти фамилий пользователей.\n" +
-        "4 - Вывести введенный список пользователей.\n" +
-        "5 - Остановка работы программы.");
-    try
+    private static void Main(string[] args)
     {
-        int choice = Convert.ToInt32(Console.ReadLine());
+        ListOfPersons newList = new();
+        UserChoice newChose = new();
+        newChose.ChoiceNumber += newList.OnUserChoiceNumber;
 
-        switch (choice)
+        while (true)
         {
-            case 1:
-                if (newList.ListIsNull() != true)
-                {
-                    newList.SortList();
-                    newList.PrintList();
-                    Console.WriteLine();
-                    Cleanconsole();
-                }
-                else
-                {
-                    Console.WriteLine("Список пользователей не заполнен! Вначале сформируйте список пользователей.");
-                    Cleanconsole();
-                }
-                break;
+            Console.WriteLine("=================================");
+            Console.WriteLine("Работа со списком пользователей:\n" +
+                "0 - Формирование массива из пяти фамилий пользователей.\n" +
+                "1 - сортировка введенного массива от А-Я\n" +
+                "2 - обратная сортировка введенного массива от Я-А\n" +
+                "3 - Вывести введенный список пользователей.\n" +
+                "4 - Остановка работы программы.");
+            Console.WriteLine("=================================");
 
-            case 2:
-                if (newList.ListIsNull() != true)
-                {
-                    newList.ReverseSort();
-                    newList.PrintList();
-                    Console.WriteLine();
-                    Cleanconsole();
-                }
-                else
-                {
-                    Console.WriteLine("Список пользователей не заполнен! Вначале сформируйте список пользователей.");
-                    Cleanconsole();
-                }
-                break;
+            try
+            {
+                Console.Write("Введите число: ");
+                newChose.Number = Convert.ToInt32(Console.ReadLine());
 
-            case 3:
-                newList.CreateList();
-                newList.PrintList();
+                switch (newChose.Number)
+                {
+                    case 0:
+                        Cleanconsole();
+                        break;
+
+                    case 1:
+                        Cleanconsole();
+                        break;
+
+                    case 2:
+                        Cleanconsole();
+                        break;
+
+                    case 3:
+                        Cleanconsole();
+                        break;
+
+                    case 4:
+                        return;
+
+                    default:
+                        Console.WriteLine("Вы ввели не верное число. Необходимо ввести либо 0, либо 1, либо 2, либо 3, либо 4.");
+                        Cleanconsole();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Необходимо ввести либо 0, либо 1, либо 2, либо 3, либо 4");
                 Cleanconsole();
-                break;
-            
-            case 4:
-                if (newList.ListIsNull() != true)
-                {
-                    newList.PrintList();
-                    Cleanconsole();
-                }
-                else
-                {
-                    Console.WriteLine("Список пользователей не заполнен! Вначале сформируйте список пользователей.");
-                    Cleanconsole();
-                }
-                break;
-
-            case 5:
-                return;
-
-
-            default:
-                Console.WriteLine("Вы ввели не верное число. Необходимо ввести либо 1, либо 2, либо 3, либо 4.");
-                Cleanconsole();
-                break;
+            }
         }
-    }
-    catch (Exception ex) 
-    { 
-        Console.WriteLine("Необходимо ввести либо 1, либо 2, либо 3, либо 4."); 
     }
 }
