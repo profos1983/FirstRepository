5b9261a7af355a26c2d5c65b690593451d45c4f2
profos1983 <jianko@hotmail.com>
2025-12-07 22:24:08 +0300

    2025-12-07 часть 3

---
+++ Task10-1-4/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task10_1_4
+{
+    internal class Interfaces
+    {
+        public interface IMamager
+        {
+            public void Create()
+            {
+            }
+
+            public void Read()
+            {
+            }
+            public void Update()
+            { 
+            }
+
+            public void Delete()
+            {
+            }
+        }
+    }
+}
---
+++ Task10-2-2/Program.cs
@@ -0,0 +1,31 @@
+﻿var writer = new Writer();
+((IWriter)writer).Write();
+
+var worker = new Worker();
+((IWorker)worker).Build();
+
+public interface IWriter
+{
+    void Write();
+}
+
+public class Writer : IWriter
+{
+    void IWriter.Write()
+    {
+
+    }
+}
+
+public interface IWorker
+{
+    public void Build();
+}
+
+public class Worker : IWorker
+{
+    public void Build()
+    {
+        throw new NotImplementedException();
+    }
+}
